f66680331c414f33d451ac746177457449f8f2a0
WillianChiquinato <willianchiquinato@hotmail.com>
2026-02-13 13:24:01 -0300

    deployment3

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ app.UseCors("AllowFrontend");
     app.UseSwaggerUI();
 // }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
